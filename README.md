c8a85269f91492c23d4d763089f8859bc9cc5c81
Anthony Vo <antvo2448@hotmail.com>
2021-08-14 00:02:00 -0400

    csv auto-updates every x time, restructured how data is stored, new promises/then for sequential loading

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# CampusNetworkMap
+# 🗺️ CampusNetworkMap
 Map visualizing the status of various Northeastern University network devices using LeafletJS (Esri Leaflet), Papa Parse, and sql.js (SQLite).
 
-### Visualization
-Markers are placed at network nodes with specific icons, a green checkmark if the network device is operational and a red X if not.
+### 📷 Screenshots
+[![Northeastern-Campus-Network-Map.png](https://i.postimg.cc/BQ9mDtGQ/Northeastern-Campus-Network-Map.png)](https://postimg.cc/MX50J6Th)
 
-### Data Resources
+### 📊 Visualization
+Markers are placed at network nodes on a map with specific icons, a green checkmark if the network device is operational and a red X if not. The map is centralized on the Northeastern University Boston campus.
+
+### 💾 Data Resources
 Northeastern Building data was manually created into a SQLite database containing the coordinates of each building. The status of each network device is updated every 5 minutes by parsing a CSV file. Map data comes from the Esri ArcGIS servers.
 
-### Credits
-Northeastern Mapping and Tiles:
+### ✍️ Additional Credits  
+Northeastern Mapping and Tiles (Esri ArcGIS):
 - Negar Pourshadi
 - Scott Margeson
 - Begum Tanriverdi Bolukbas
@@ -16,8 +19,8 @@ Northeastern Mapping and Tiles:
 Checkmarks
 - Freepik
 
-### TODO:
-- Update the map periodically (every 5 minutes)
+### 📝 TODO:
+- Update the map periodically (every 5 minutes) (DONE)
   - Include a "Time last updated" indicator
 - Search bar for markers
 - Table of all/non-functional network devices
--- js/MarkerStatus.js
+++ js/MarkerStatus.js
@@ -8,11 +8,19 @@
 * The format of the CSV file is as follows: Name, x-coordinate, y-coordinate, status.
 * 
 * TODO: parse extra information into bullet-point pop
-* TODO: have this command run every time the CSV is updated or every "x" minutes.
+*/
+
+/*
+  _____ _____ ____  _   _  _____ 
+ |_   _/ ____/ __ \| \ | |/ ____|
+   | || |   | |  | |  \| | (___  
+   | || |   | |  | | . ` |\___ \ 
+  _| || |___| |__| | |\  |____) |
+ |_____\_____\____/|_| \_|_____/                                                
 */
 
 /**
-* Base Icon model - svg icons are sized as 20x20
+* Base Icon template - svg icons are sized as 20x20.
 */
 var StatusIcon = L.Icon.extend({
   options: {
@@ -24,71 +32,102 @@ var StatusIcon = L.Icon.extend({
 });
 
 /**
-* Good (working) and bad (not working) icons to be used as markers
+* Good icon representing a functional network device. To be used as a marker icon.
 */
-var goodIcon = new StatusIcon({ iconUrl: "images/check-mark.svg" }),
-  badIcon = new StatusIcon({ iconUrl: "images/x-mark.svg" });
+var goodIcon = new StatusIcon({ iconUrl: "images/check-mark.svg" });
+/**
+ * Bad icon representing a non-responsive network device. To be used as a marker icon.
+ */
+var badIcon = new StatusIcon({ iconUrl: "images/x-mark.svg" });
+
+/*
+  _      ____          _____ _____ _   _  _____    _____       _______       
+ | |    / __ \   /\   |  __ \_   _| \ | |/ ____|  |  __ \   /\|__   __|/\    
+ | |   | |  | | /  \  | |  | || | |  \| | |  __   | |  | | /  \  | |  /  \   
+ | |   | |  | |/ /\ \ | |  | || | | . ` | | |_ |  | |  | |/ /\ \ | | / /\ \  
+ | |___| |__| / ____ \| |__| || |_| |\  | |__| |  | |__| / ____ \| |/ ____ \ 
+ |______\____/_/    \_\_____/_____|_| \_|\_____|  |_____/_/    \_\_/_/    \_\                                                         
+*/
 
 /** 
- * Map of network devices and their statuses
+ * Map of network devices and their statuses.
+ * The format of the map is: key=Device, value=data
  */
 var csvData = new Map();
 
 /**
-* parseCSV() takes in the directory of the CSV file. It stores the data
+ * List of markers placed onto the map.
+ */
+var markerGroup = L.layerGroup().addTo(map);
+
+/**
+* loadCSV() takes in the directory of the CSV file and stores the data
 * into a map called csvData.
 * 
-* Parse CSV (containing network data) using Papa Parser into an array.
+* Parse CSV (containing network data) using Papa Parse into an array.
 * Papa.parse creates an object with 3 elements: data, errors, and meta.
 * The data is an array of Objects that represents each row of the CSV.
 *
-* @param {*} location the file location of the CSV file.
+* @param {String} location the file location of the CSV file.
 */
-function parseCSV(location) {
-  Papa.parse(location, {
-    header: true,
-    download: true,
-    dynamicTyping: true,
-    step: function (row) {
-      // Create a Map of the CSV
-      csvData.set(row.data.Device, row.data);
-    }
-  });
-
+function loadCSV(location) {
   // Promise determines if the function has finished running yet.
   return new Promise(function (resolve, reject) {
-    resolve('Parse CSV done.')
+
+    Papa.parse(location, {
+      header: true,
+      download: true,
+      dynamicTyping: true,
+      step: function (row) {
+        csvData.set(row.data.Device, row.data); // Create a Map of the CSV
+      },
+      complete: function (results) {
+        resolve('Parse CSV done.') // Resolve = done and promise fulfilled.
+      }
+    });
   })
 }
 
 /**
  * Load a SQLite file from path.
- * @param {*} path the location of the SQLite file.
+ * 
+ * loadSQLite utilizes sql.js functionality in order to mimick SQLite functionality
+ * in JS. It creates a var db database that can be accessed using .exec with SQL
+ * functions.
+ * 
+ * @param {String} path the location of the SQLite file.
  */
 function loadSQLite(path) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', path, true);
-  xhr.responseType = 'arraybuffer';
-
-  // once the file has been loaded, parse it:
-  xhr.onload = e => {
-    const uInt8Array = new Uint8Array(xhr.response);
-    const db = new SQL.Database(uInt8Array);
-
-    // Call helper method to place markers.
-    placeMarkerSQL(db);
-  };
-  xhr.send();
-
-  // Promise determines if the function has finished running yet.
   return new Promise(function (resolve, reject) {
-    resolve('Load SQLite done.')
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'arraybuffer';
+
+    // once the file has been loaded, create the array (database) and return it (resolve)
+    xhr.onload = function () {
+      const uInt8Array = new Uint8Array(xhr.response);
+      resolve(new SQL.Database(uInt8Array)); // Resolve = done and promise fulfilled.
+    };
+
+    xhr.onerror = reject; // if something wrong happens, return an error for promise
+    xhr.send();
   })
 }
 
+/*
+  __  __          _____     __  __          _____  _  ________ _____   _____ 
+ |  \/  |   /\   |  __ \   |  \/  |   /\   |  __ \| |/ /  ____|  __ \ / ____|
+ | \  / |  /  \  | |__) |  | \  / |  /  \  | |__) | ' /| |__  | |__) | (___  
+ | |\/| | / /\ \ |  ___/   | |\/| | / /\ \ |  _  /|  < |  __| |  _  / \___ \ 
+ | |  | |/ ____ \| |       | |  | |/ ____ \| | \ \| . \| |____| | \ \ ____) |
+ |_|  |_/_/    \_\_|       |_|  |_/_/    \_\_|  \_\_|\_\______|_|  \_\_____/ 
+*/
+
 /**
- * Places the network data as nodes onto the map.
- * @param {*} db the database containing network data.
+ * Places the network data as nodes onto the marker group (which is placed onto the map).
+ * 
+ * @param {Array} db the database containing network data.
  */
 function placeMarkerSQL(db) {
   // .exec stores the result in an array of objects (numbered 0 to ...)
@@ -103,7 +142,7 @@ function placeMarkerSQL(db) {
     var marker = L.marker([row[1], row[2]],
       { icon: determineIcon(row[3]) }) // icon is good if all network devices are on for "x" building
       .bindPopup(row[0])
-      .addTo(map);
+      .addTo(markerGroup);
   }
 }
 
@@ -111,13 +150,12 @@ function placeMarkerSQL(db) {
  * Determines the icon to display for a building based on the state of the network devices 
  * in said building.
  * 
- * @param {*} listOfDevices a string of all the devices assigned to a building.
+ * @param {String} listOfDevices a string of all the devices assigned to a building.
  * @returns the icon to use for the node on the map.
  */
 function determineIcon(listOfDevices) {
   // Split the list of devices (comma-delimited, auto-trims extra spaces)
   // Example: accs-tf-133-1,accs-tf-173a-1,accs-tf-265b-1,accs-tf-r312-1,accs-tf-r410-1,accs-tf-x411-1
-
   var deviceArray = listOfDevices.split(/\s*,\s*/);
 
   // for each device in the device array (associated with a building), make sure all devices are on.
@@ -131,14 +169,51 @@ function determineIcon(listOfDevices) {
   return goodIcon;
 }
 
-// Start parsing and plotting network points.
+/*
+  _____  _    _ _   _ _   _ _____ _   _  _____             _   _ _____  
+ |  __ \| |  | | \ | | \ | |_   _| \ | |/ ____|      /\   | \ | |  __ \ 
+ | |__) | |  | |  \| |  \| | | | |  \| | |  __      /  \  |  \| | |  | |
+ |  _  /| |  | | . ` | . ` | | | | . ` | | |_ |    / /\ \ | . ` | |  | |
+ | | \ \| |__| | |\  | |\  |_| |_| |\  | |__| |   / ____ \| |\  | |__| |
+ |_|  \_\\____/|_| \_|_| \_|_____|_| \_|\_____|  /_/    \_\_| \_|_____/ 
+  _    _ _____  _____       _______ _____ _   _  _____                  
+ | |  | |  __ \|  __ \   /\|__   __|_   _| \ | |/ ____|                 
+ | |  | | |__) | |  | | /  \  | |    | | |  \| | |  __                  
+ | |  | |  ___/| |  | |/ /\ \ | |    | | | . ` | | |_ |                 
+ | |__| | |    | |__| / ____ \| |   _| |_| |\  | |__| |                 
+  \____/|_|    |_____/_/    \_\_|  |_____|_| \_|\_____|                                    
+*/
 
 /**
  * Runs the code in sequential order. The CSV MUST be parsed first, then the SQLite db is loaded
  * and parsed appropriately.
  */
 function run() {
-  parseCSV('data/exampleAkipsStatus2.csv')
-    .then(loadSQLite('data/buildings.db'));
+  // Add markers
+  loadCSV('data/exampleAkipsStatus2.csv') // load CSV first
+    .then(() => loadSQLite('data/buildings.db')) // then load SQLite db (order is important for return result)
+    .then(function (database) { // database = the returned promise from loadSQLite
+      placeMarkerSQL(database);
+
+      // Run update() every 2000ms (2 seconds).
+      var i = setInterval(function () { update(database); }, 2000);
+    });
 }
+
+/**
+ * Updates the map periodically.
+ * 
+ * @param {Array} db the SQLite database.
+ */
+function update(db) {
+
+  // Clear all markers
+  markerGroup.clearLayers();
+  // // reload CSV data
+  loadCSV('data/exampleAkipsStatus2.csv')
+    //   // place new markers
+    .then(() => placeMarkerSQL(db))
+}
+
+// START
 run();
